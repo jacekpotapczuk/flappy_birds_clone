5429718fbe9a4318078da005178a25cdbfc4405f
jacekpotapczuk <jacekpotapczuk@gmail.com>
2021-01-03 23:01:09 +0100

    add basic gameplay
    2h/24h

---
+++ Assets/Scripts/PipesControler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PipesControler : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)]
+    private float scrollSpeed = 1f;
+
+    [SerializeField] private Transform[] pipes;
+    
+    private const float leftCutoff = -3.5f;
+    private const float rightCutoff = 3.5f;
+    
+    private void Update()
+    {
+        foreach (Transform pipe in pipes)
+        {
+            Vector3 pos = pipe.position;
+            pos.x -= scrollSpeed * Time.deltaTime;
+            pipe.position = pos;
+        }
+        
+        // Nastpene kroki:
+        // odpowiednie zapętlenie pipes
+        // skalowanie sie poziopmu trudnosci
+        // umieranie 
+        // score i wyswiuetlanie na ekranie
+        // dodanie assetow
+        // game menus
+        // dzwiek
+        // dodatkowe efekty wizualne
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField, Range(0f, 20f)]
+    private float clickForce = 5f;
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        Physics2D.gravity = new Vector3(0f, -20f, 0f);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.localPosition = new Vector3(-2f, 1.5f, 0f);
+        }
+        else if (Input.anyKeyDown)
+        {
+            rigidbody.velocity = new Vector2(0f, clickForce);
+        }
+    }
+}
