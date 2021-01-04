618951cc77083e4cdc69ed86cfbe77a522fa4ec4
jacekpotapczuk <jacekpotapczuk@gmail.com>
2021-01-04 15:29:16 +0100

    add ui elements, godMode, death
    4h/24h

--- Assets/Scripts/PipesControler.cs
+++ Assets/Scripts/PipesControler.cs
@@ -1,34 +1,68 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class PipesControler : MonoBehaviour
 {
     [SerializeField, Range(0f, 10f)]
     private float scrollSpeed = 1f;
-
+    
+    
     [SerializeField] private Transform[] pipes;
+
+    [SerializeField] private Player player;
     
     private const float leftCutoff = -3.5f;
-    private const float rightCutoff = 3.5f;
+    private const float rightCutoff = 3.5f; // TODO: usunac jezeli okaze sie useless
+    private float pipeDistance = 4f; // TODO: skalowanie z poziomem trudnosci, pamietac o zachowaniu odpowiednich odl przy zmiane w runtime
+
+    private bool[] didPipeScore;
+    
+    private float heightMax = 2f;
+
+    private float heightMin = -3f;
+    
+    private void Start()
+    {
+        didPipeScore = new bool[pipes.Length];
+        Restart();
+    }
     
     private void Update()
     {
-        foreach (Transform pipe in pipes)
+        for (int i = 0; i < pipes.Length; i++)
         {
+            Transform pipe = pipes[i];
+            if (pipe.position.x <= -3f && !didPipeScore[i])
+            {
+                player.AddScore();
+                didPipeScore[i] = true;
+            }
+                
+            
             Vector3 pos = pipe.position;
             pos.x -= scrollSpeed * Time.deltaTime;
+
+            if (pos.x < leftCutoff)
+            {
+                pos.x += 2f * pipeDistance;
+                pos.y = Random.Range(heightMin, heightMax);
+                didPipeScore[i] = false;
+            }
+                
             pipe.position = pos;
         }
+    }
+
+    public void Restart()
+    {
+        pipes[0].position = new Vector3(3.5f, 0f, 0f); // init first pipe
         
-        // Nastpene kroki:
-        // odpowiednie zapętlenie pipes
-        // skalowanie sie poziopmu trudnosci
-        // umieranie 
-        // score i wyswiuetlanie na ekranie
-        // dodanie assetow
-        // game menus
-        // dzwiek
-        // dodatkowe efekty wizualne
+        // initialize rest of pipes to keep correct distance from the first pipe
+        for (int i = 1; i < pipes.Length; i++)
+        {
+            Vector3 pos = pipes[i].position;
+            pos.x = pipes[i - 1].position.x + pipeDistance;
+            pipes[i].position = pos;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
-
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
 public class Player : MonoBehaviour
 {
+    [SerializeField] private PipesControler pipesControler;
+    
     [SerializeField, Range(0f, 20f)]
     private float clickForce = 5f;
+
+    [SerializeField] private Text scoreText;
+    private bool godMode = false;
+    private int score = 0;
 
     private Rigidbody2D rigidbody;
 
@@ -18,13 +26,38 @@ public class Player : MonoBehaviour
     private void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.localPosition = new Vector3(-2f, 1.5f, 0f);
-        }
-        else if (Input.anyKeyDown)
+        if (Input.anyKeyDown && EventSystem.current.currentSelectedGameObject == null)
         {
             rigidbody.velocity = new Vector2(0f, clickForce);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (godMode)
+            return;
+                
+        transform.localPosition = new Vector3(-2f, 1.5f, 0f);
+        score = 0;
+        UpdateScoreText();
+        pipesControler.Restart();
+    }
+
+
+    public void ToggleGodMode(bool b)
+    {
+        godMode = b;
+    }
+
+    public void AddScore()
+    {
+        score += 1;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+    
 }
