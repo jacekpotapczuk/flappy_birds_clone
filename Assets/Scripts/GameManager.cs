0e2bc68fc431700a88db92e6933ba6ae49fb6a13
jacekpotapczuk <jacekpotapczuk@gmail.com>
2021-01-06 15:10:33 +0100

    add title
    13h/24h

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        //LoadMainMenu();
+        LoadMainMenu();
 
         SceneManager.sceneLoaded += this.OnSceneLoaded;
     }
